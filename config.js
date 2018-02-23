cd7f9a4ca1fb58ece6e31fff0a01b94a3a90de2e
alexandraburress <alexandra.burress@ndsu.edu>
2018-02-23 12:58:42 -0600

    begin

---
+++ config.js
@@ -0,0 +1,9 @@
+var twit = require('twit');
+
+module.exports = {
+    consumer_key: 'fScQe4FPFfIOp0jEUiZfUlBAn',
+    consumer_secret: 'Jed9dpEKWFyGhhZhRqUmAL1nmjNlF71aQyKkTE2BoUubm92N2v',
+    access_token: '963829279388848128-wIWgeU8XjRw34AeHd0mqJIBU4wLHh4A',
+    access_token_secret: 'Udizv1rbYG6MQgmTDPwiPRdzTzt7oHWASFfAM3G3JNiqJ',
+
+}
